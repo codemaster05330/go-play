a1af220d79d9dc49a86db349b30e5fcde1e026f7
codemaster05330 <thomasj0830@gmail.com>
2018-08-02 20:26:22 +0530

    minor chagnes.

--- url-path-seg/server/server.go
+++ url-path-seg/server/server.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	log.Println("server started at localhost:8080")
+	fmt.Println("server started at localhost:8080")
 
 	http.Handle("/", http.HandlerFunc(handler))
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(rw http.ResponseWriter, req *http.Request) {
-	log.Printf("req.URL.Path=%s\n", req.URL.Path)
-	log.Printf("req.URL.RawQuery=%s\n", req.URL.RawQuery)
-	log.Printf("w=%s\n", req.URL.Query().Get("w"))
+	fmt.Printf("req.URL.Path=%s\n", req.URL.Path)
+	fmt.Printf("req.URL.RawQuery=%s\n", req.URL.RawQuery)
+	printQueryParam(req.URL.Query())
+	printHeaders(req.Header)
+
+	writePods(rw)
+}
+
+func printQueryParam(params url.Values) {
+	fmt.Printf("query_params %d\n", len(params))
+	for key, value := range params {
+		fmt.Printf("key=%s, value=%s\n", key, value)
+	}
+}
+
+func printHeaders(headers http.Header) {
+	fmt.Printf("headers %d\n", len(headers))
+	for key, value := range headers {
+		fmt.Printf("key=%s, value=%s\n", key, value)
+	}
+}
+
+func writePods(rw http.ResponseWriter) {
+	res := `{"kind":"PodList","apiVersion":"v1","metadata":{"selfLink":"/api/v1/namespaces/john-preview-stage/pods","resourceVersion":"300455571"},"items":[{"metadata":{"name":"cpl-tools-1-62d9w","generateName":"cpl-tools-1-","namespace":"nvirani-preview-stage","uid":"e2a13ac3-9432-11e8-bf1d-029a5a55534e","creationTimestamp":"2018-07-30T19:58:10Z"}},{"metadata":{"name":"dummy-app1-1-6tb8l","generateName":"dummy-app1-1-","namespace":"john-preview-stage","uid":"772af236-9432-11e8-bf1d-029a5a55534e","creationTimestamp":"2018-07-30T19:55:10Z"}}]}`
+	rw.Write([]byte(res))
 }
 
 /*
@@ -29,7 +52,7 @@ sample run
 2018/07/31 17:15:59 w=true
 
 ** client output **
-[nvirani@localhost client]$ go run client.go
+[user1@localhost client]$ go run client.go
 2018/07/31 17:15:59 req.URL.Path=/api/v1/namespaces/ns;type=che/pods/p;space=997f146d-b0f4-4a97-ab20-6414878d9508
 2018/07/31 17:15:59 req.URL.RawQuery=w=true
 2018/07/31 17:15:59 res.StatusCode=200
